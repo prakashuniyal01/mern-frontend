f69dd5abe24ac4178cf12621bf67b7dffda420e3
prakashuniyal01 <uniyalprakash7@gmail.com>
2024-03-19 16:21:48 +0530

    update

--- src/components/GeneralScreens/Footer.js
+++ src/components/GeneralScreens/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="footer">
             </div>
             <div className="copyright">
-                <p className="copyright-blog">© 2022 Gilbert Hutapea. All Rights Reserved</p>
+                <p className="copyright-blog">© 2024 Blog by prakash rawat | <a href="https://prakash-uniyal.onrender.com/">prakash-uniyal</a> | All Rights Reserved</p>
             </div>
         </div>
     )
